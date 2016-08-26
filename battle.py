16645f0f2895694b476d0f43320ac5548e258b07
ignite-the-vibe <verbzak@gmail.com>
2016-08-26 12:36:08 -0500

    Add files via upload

---
+++ battle.py
@@ -0,0 +1,18 @@
+#Battle
+
+
+
+p_attacks = ["Hit", "Stun", "Kick"]
+p_hp = 100
+
+in p_attacks "Hit" = 20
+in p_attacks "Stun" = 15
+in p_attacks "Kick" = 25
+
+
+c_attacks = ["Burn", "Freeze", "Flick"]
+c_hp = 100
+
+in c_attacks "Burn" = 25
+in c_attacks "Freeze" = 20
+in c_attacks "Flick" = 5
